1c6b15a11fe0dd7c8c9703af01a312019145816e
sauld@cimat.mx <saul.diazinfante@unison.mx>
2022-07-03 20:22:48 -0700

    with tomato preprint

--- _site/assets/themes/j1/adapter/js/attic.js
+++ _site/assets/themes/j1/adapter/js/attic.js
@@ -17,7 +17,7 @@
  # Backstretch is licensed under the MIT License.
  # For details, see https://github.com/jquery-backstretch/jquery-backstretch
  # -----------------------------------------------------------------------------
- #  Adapter generated: 2022-07-03 04:45:59 +0200
+ #  Adapter generated: 2022-07-04 05:17:22 +0200
  # -----------------------------------------------------------------------------
 */
 // -----------------------------------------------------------------------------
@@ -45,7 +45,7 @@ j1.adapter.attic = (function (j1, window) {
       // -----------------------------------------------------------------------
       var settings = $.extend({
         module_name: 'j1.adapter.attic',
-        generated:   '2022-07-03 04:45:59 +0200'
+        generated:   '2022-07-04 05:17:22 +0200'
       }, options);
       // -----------------------------------------------------------------------
       // Global variable settings

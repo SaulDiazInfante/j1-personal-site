00c6e53c151110bf9eaef032d17295287dcf1a7f
sauld@cimat.mx <saul.diazinfante@unison.mx>
2022-07-13 10:28:05 -0700

    With new layout for bibdata

--- _site/assets/themes/j1/adapter/js/carousel.js
+++ _site/assets/themes/j1/adapter/js/carousel.js
@@ -13,7 +13,7 @@
  # J1 Template is licensed under the MIT License.
  # See: https://github.com/jekyll-one-org/J1 Template/blob/master/LICENSE
  # -----------------------------------------------------------------------------
- #  Adapter generated: 2022-07-13 00:34:13 +0200
+ #  Adapter generated: 2022-07-13 19:23:35 +0200
  # -----------------------------------------------------------------------------
 */
 // -----------------------------------------------------------------------------
@@ -45,7 +45,7 @@ j1.adapter.carousel = (function (j1, window) {
       // -----------------------------------------------------------------------
       var settings = $.extend({
         module_name: 'j1.adapter.carousel',
-        generated:   '2022-07-13 00:34:13 +0200'
+        generated:   '2022-07-13 19:23:35 +0200'
       }, options);
       // -----------------------------------------------------------------------
       // Global variable settings
--- _site/assets/themes/j1/adapter/js/cookieConsent.js
+++ _site/assets/themes/j1/adapter/js/cookieConsent.js
@@ -12,7 +12,7 @@
  #  J1 Template is licensed under MIT License.
  #  See: https://github.com/jekyll-one/J1 Template/blob/master/LICENSE
  # -----------------------------------------------------------------------------
- #  Adapter generated: 2022-07-13 00:34:13 +0200
+ #  Adapter generated: 2022-07-13 19:23:35 +0200
  # -----------------------------------------------------------------------------
 */
 // -----------------------------------------------------------------------------
@@ -64,7 +64,7 @@ j1.adapter.cookieConsent = (function (j1, window) {
       // -----------------------------------------------------------------------
       var settings = $.extend({
         module_name: 'j1.adapter.cookieConsent',
-        generated:   '2022-07-13 00:34:13 +0200'
+        generated:   '2022-07-13 19:23:35 +0200'
       }, options);
       // -----------------------------------------------------------------------
       // Global variable settings

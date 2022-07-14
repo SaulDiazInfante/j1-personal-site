5ee793b5655ccf738f529aecf2e9a9caff98bfdb
sauld@cimat.mx <saul.diazinfante@unison.mx>
2022-07-13 18:38:25 -0700

    Persistence theory.TODO: remark

--- _site/assets/themes/j1/adapter/js/algolia.js
+++ _site/assets/themes/j1/adapter/js/algolia.js
@@ -13,7 +13,7 @@
  # J1 Template is licensed under the MIT License.
  # For details, see: https://github.com/jekyll-one-org/j1-template/blob/main/LICENSE.md
  # -----------------------------------------------------------------------------
- #  Adapter generated: 2022-07-13 19:23:35 +0200
+ #  Adapter generated: 2022-07-14 02:12:16 +0200
  # -----------------------------------------------------------------------------
 */
 // -----------------------------------------------------------------------------
@@ -44,7 +44,7 @@ j1.adapter.algolia = (function (j1, window) {
       // -----------------------------------------------------------------------
       var settings = $.extend({
         module_name: 'j1.adapter.algolia',
-        generated:   '2022-07-13 19:23:35 +0200'
+        generated:   '2022-07-14 02:12:16 +0200'
       }, options);
       // -----------------------------------------------------------------------
       // Global variable settings

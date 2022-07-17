2ac643a2294906d0de081b80fe2626a0489af9e5
sauld@cimat.mx <saul.diazinfante@unison.mx>
2022-07-16 20:31:12 -0700

    Persistence theory. TODO: ref

--- _site/assets/themes/j1/adapter/js/asciidoctor.js
+++ _site/assets/themes/j1/adapter/js/asciidoctor.js
@@ -13,7 +13,7 @@
  # J1 Template is licensed under the MIT License.
  # For details, see: https://github.com/jekyll-one-org/j1-template/blob/main/LICENSE.md
  # -----------------------------------------------------------------------------
- #  Adapter generated: 2022-07-14 20:37:49 +0200
+ #  Adapter generated: 2022-07-17 05:03:25 +0200
  # -----------------------------------------------------------------------------
 */
 // -----------------------------------------------------------------------------
@@ -44,7 +44,7 @@ j1.adapter.asciidoctor = (function (j1, window) {
       // -----------------------------------------------------------------------
       var settings = $.extend({
         module_name: 'j1.adapter.rtable',
-        generated:   '2022-07-14 20:37:49 +0200'
+        generated:   '2022-07-17 05:03:25 +0200'
       }, options);
       // -----------------------------------------------------------------------
       // Global variable settings
